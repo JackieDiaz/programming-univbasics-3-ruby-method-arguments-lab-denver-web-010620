2d2c2a4d50bcd239fd75130f82b198edf8d33235
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-18 04:20:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,11 @@ puts "Hi, my name is #{name}."
 end
 introduction("peter")
 introduction("paul")
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("peter", "Ruby")
+introduction_with_language)"paul", "Java")
 
 
 

69ed391ae3c3d759bd44cbd75045193f0bdf4fd0
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-18 04:24:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,8 @@ end
 introduction_with_language("peter", "Ruby")
 introduction_with_language)"paul", "Java")
 def introduction_with_language_optional(name, language = "Ruby")
-
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
